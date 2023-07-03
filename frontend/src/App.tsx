72ef59a4f18fa167d85f06f4fa13930074ccf966
John J Rodriguez <87795271+JohnRodriguezM@users.noreply.github.com>
2023-07-02 22:15:17 -0500

    socket connection is ready

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,77 @@
-import './css/App.css'
-import io from 'socket.io-client'
+import { useEffect, useState } from "react";
+import "./css/App.css";
+import io from "socket.io-client";
 
-const socket = io('http://localhost:3000')
+const socket = io("http://localhost:3000");
+
+export const App = () => {
+  const [message, setMessage] = useState<string>("");
+  const [received, setReceivedMessages] = useState<string[]>([]);
+
+  useEffect(() => {
+    // the other clients are listening to the "message" event
+    socket.on("message", (message: string) => {
+        console.log(message);
+    });
+
+    socket.on("message", (message: string) => {
+      setReceivedMessages((received) => [...received, message]);
+    });
+  }, []);
 
 
-export const App = () =>{
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    // console.log(message);
+    // Send message to server
+    socket.emit("message", message);
+    setMessage("");
+    // Receive message from server
+
+
+
+  };
 
   return (
     <>
-      <h1>React 18 Alpha</h1>
-    </>
-  )
-}
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => {
+            setMessage(e.target.value);
+          }}
+          value={message}
+          type="text"
+          placeholder="Enter your message"
+        />
+        <button>Send</button>
+      </form>
 
+      <div className="messages">
+        {received.map((message, index) => {
+          return <div key={index}>{message}</div>;
+        })}
+      </div>
+
+      <button
+        style={{
+          position: "absolute",
+          bottom: "0",
+          right: "0",
+          margin: "10px",
+          padding: "10px",
+          backgroundColor: "red",
+          color: "white",
+          borderRadius: "10px",
+          border: "none",
+          cursor: "pointer",
+        }}
+        onClick={() => {
+          setReceivedMessages([]);
+        }}
+      >
+        Clean Chat
+      </button>
+    </>
+  );
+};
